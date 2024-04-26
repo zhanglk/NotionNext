56fac568b0375a1e308dae960a013da25a131b86
zhanglk <zhanglk@gmail.com>
2024-04-26 21:15:53 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.lightslord.top' },
-    { title: '访问文档中心获取更多乐趣', url: 'http://book.lightslord.com' }
+    { title: '访问图书中心获取更多乐趣', url: 'http://book.lightslord.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
